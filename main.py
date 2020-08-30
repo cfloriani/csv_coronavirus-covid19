4257e597a5d90052a9090bf9de5cf497c6664d9b
Carlos Eduardo <60077621+cfloriani@users.noreply.github.com>
2020-08-30 02:05:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from urllib.request import urlopen
 
 arq = urlopen('https://api.covid19api.com/all').read().decode('utf8') #usa api como fonte de dados
 fulldata = json.loads(arq)
-data = {} #{'País':(Deaths,Confirmed,Recovered)}
+dados = {} #{'País':(Deaths,Confirmed,Recovered)}
 
 #cria os indices com os paises
-for x in fulldata:
-	if x['Country'] not in data:
-		data[x['Country']] = [0,0,0]
+for dado in fulldata:
+	if dado['Country'] not in dados:
+		dados[x['Country']] = [0,0,0]
 
 #verifica país a país os casos
-for x in fulldata:
-	if x['Status'] == 'deaths':
-		if x['Cases'] > data[x['Country']][0]:
-			data[x['Country']][0] = x['Cases']
-	if x['Status'] == 'confirmed':
-		if x['Cases'] > data[x['Country']][1]:
-			data[x['Country']][1] = x['Cases']
-	if x['Status'] == 'recovered':
-		if x['Cases'] > data[x['Country']][2]:
-			data[x['Country']][2] = x['Cases']
+for dado in fulldata:
+	if dado['Status'] == 'deaths':
+		if dado['Cases'] > dados[dado['Country']][0]:
+			dados[dado['Country']][0] = dado['Cases']
+	if dado['Status'] == 'confirmed':
+		if dado['Cases'] > dados[dado['Country']][1]:
+			dados[dado['Country']][1] = dado['Cases']
+	if dado['Status'] == 'recovered':
+		if dado['Cases'] > dados[dado['Country']][2]:
+			dados[dado['Country']][2] = dado['Cases']
 
 #imprime no csv
 with open('export.csv','w') as arquivo:
 	writer = csv.writer(arquivo)
 	writer.writerow(('País', 'Mortes', 'Infectados','Recuperados'))
-	for i in data:
-		writer.writerow((i,data[i][0],data[i][1],data[i][2]))
+	for i in dados:
+		writer.writerow((i,dados[i][0],dados[i][1],dados[i][2]))
